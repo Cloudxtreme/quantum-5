9c2ad00e7fa8fa34a19514f53d2077a4f8ad5b56
Gabooovev <atomaki@gmail.com>
2013-10-01 17:29:21 +0200

    modified index

--- web/index.php
+++ web/index.php
@@ -1,2 +1,12 @@
 <?php
-include 'includes/bootstrap.php';
+// Futasido inditasa.
+define('SCRIPT_START', microtime(true));
+
+// Alapertelemezett include mappas.
+set_include_path(dirname(__DIR__));
+
+// Session inditasa.
+session_start();
+
+// Bootolas.
+include('../includes/bootstrap.php');
