f0deb77d9c5b2b1ce57d9d5fb5f4eae25f5e7655
Gabooovwv <atomaki@gmail.com>
2013-10-06 22:09:25 +0200

    add file

---
+++ cache/classes.php
@@ -0,0 +1,26 @@
+<?php
+
+return array (
+  'Request' => 'libs/Quantum.php',
+  'Cache' => 'libs/Quantum.php',
+  'DbFactory' => 'libs/Quantum.php',
+  'Mysql' => 'libs/Quantum.php',
+  'DbResult' => 'libs/Quantum.php',
+  'Template' => 'libs/Quantum.php',
+  'View' => 'libs/Quantum.php',
+  'Site' => 'libs/Quantum.php',
+  'PageController' => 'libs/Quantum.php',
+  'Config' => 'libs/Quantum.php',
+  'Logger' => 'libs/Quantum.php',
+  'IndexController' => 'Controller/IndexController.php',
+  'PortalController' => 'Controller/PortalController.php',
+  'SystemHelper' => 'includes/SystemHelper.php',
+  'DateHelper' => 'includes/DateHelper.php',
+  'SystemBo' => 'includes/Bo/SystemBo.php',
+  'SystemDao' => 'includes/Dao/SystemDao.php',
+  'Session' => 'includes/Session.php',
+  'UrlHelper' => 'includes/UrlHelper.php',
+  'FormHelper' => 'includes/FormHelper.php',
+  'TranslateHelper' => 'includes/TranslateHelper.php',
+  'EmailHelper' => 'includes/EmailHelper.php',
+);
